5bb13e1c65c78909a75f8e5f7b6f05a9d92b7dcc
naomiajacobs <naomiajacobs@gmail.com>
2016-08-14 03:16:45 -0700

    Fix model columns, associate schools and teams

--- db/migrate/20160814020248_create_schools.rb
+++ db/migrate/20160814020248_create_schools.rb
@@ -1,7 +1,7 @@
 class CreateSchools < ActiveRecord::Migration[5.0]
   def change
     create_table :schools do |t|
-      t.string :school
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20160814025542_create_teams.rb
+++ db/migrate/20160814025542_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
-      t.string :team
+      t.integer :number
 
       t.timestamps
     end
