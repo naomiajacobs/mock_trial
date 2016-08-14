1c572c919b1d4e375fbdf353a5ad480fa4701148
naomiajacobs <naomiajacobs@gmail.com>
2016-08-13 19:56:09 -0700

    Add teams model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814021241) do
+ActiveRecord::Schema.define(version: 20160814025542) do
 
   create_table "schools", force: :cascade do |t|
     t.string   "school"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "team"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
