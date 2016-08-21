5405ceec9535ed663a5b58c07f45e9f55c26a77a
naomiajacobs <naomiajacobs@gmail.com>
2016-08-21 02:27:59 -0700

    Automate theme generation; use example card to test themes

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -3,11 +3,82 @@ import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { cyanDeepPurple } from 'themes'
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+class CardControlled extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  handleExpandChange(expanded) {
+    this.setState({expanded: expanded});
+  }
+
+  handleToggle(event, toggle) {
+    this.setState({expanded: toggle});
+  }
+
+  handleExpand() {
+    this.setState({expanded: true});
+  }
+
+  handleReduce() {
+    this.setState({expanded: false});
+  }
+
+  render() {
+    return (
+      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange.bind(this)}>
+        <CardHeader
+          title="URL Avatar"
+          subtitle="Subtitle"
+          avatar="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png"
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText>
+          <Toggle
+            toggled={this.state.expanded}
+            onToggle={this.handleToggle.bind(this)}
+            labelPosition="right"
+            label="This toggle controls the expanded state of the component."
+          />
+        </CardText>
+        <CardMedia
+          expandable={true}
+          overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+        >
+          <img src="http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_ukaavUI.jpg" />
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" expandable={true} />
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+          <FlatButton label="Expand" onTouchTap={this.handleExpand.bind(this)} />
+          <FlatButton label="Reduce" onTouchTap={this.handleReduce.bind(this)} />
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={cyanDeepPurple}>
-        <AppBar title="Hello World!" />
+        <div>
+          <AppBar title="Hello World!" />
+          <CardControlled />
+        </div>
       </MuiThemeProvider>
     )
   }
