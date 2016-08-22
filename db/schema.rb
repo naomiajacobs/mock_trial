0d73ee4f5dbb3909d57669389415e51f9162760d
naomiajacobs <naomiajacobs@gmail.com>
2016-08-21 18:54:39 -0700

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814101945) do
+ActiveRecord::Schema.define(version: 20160822015159) do
 
   create_table "ballots", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -96,6 +96,13 @@ ActiveRecord::Schema.define(version: 20160814101945) do
     t.datetime "updated_at", null: false
     t.date     "date"
     t.boolean  "coin_flip"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
