27a559fe23990b7b7a2bc87783c01ed9b794dfc5
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-29 01:58:24 -0700

    Wipe DB clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814021241) do
-
-  create_table "schools", force: :cascade do |t|
-    t.string   "school"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170729084934) do
 
 end
